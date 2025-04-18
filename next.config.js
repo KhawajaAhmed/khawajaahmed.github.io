4e1bb403cd7ee91efa67a2f3e52cec461e9397be
Khawaja Hussain Ahmed <khawajaahmed_2025@depauw.edu>
2025-04-18 16:40:05 -0400

    Added support for GitHub pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Adjust this to match your GitHub repository name if not using username.github.io
+  // basePath: '/portfolio_3',
 };
 
 module.exports = nextConfig;
